465c3f29a517bfcb24e66d11d4552ea472b022d5
SupremeSalton <arturalesh@gmail.com>
2023-06-24 13:59:32 +0700

    Add rout to main page

--- src/pages/Login/login.tmpl.js
+++ src/pages/Login/login.tmpl.js
@@ -10,7 +10,7 @@ export const template =  `
         </form>
     </main>
 
-    <ul class="link_list">
+    <ul class="link_list"> For Netlify:
         <li> <a href="https://dreamy-chaja-f6b77c.netlify.app/login"> Login (/login)</a></li>
         <li> <a href="https://dreamy-chaja-f6b77c.netlify.app/registration"> Registration (/registration) </a></li>
         <li> <a href="https://dreamy-chaja-f6b77c.netlify.app/chats"> Chats (/chats) </a></li>
@@ -19,5 +19,15 @@ export const template =  `
         <li> <a href="https://dreamy-chaja-f6b77c.netlify.app/passwordchange"> Change Password (/passwordchange) </a></li>
         <li> <a href="https://dreamy-chaja-f6b77c.netlify.app/404"> 404 (/404)</a></li>
         <li> <a href="https://dreamy-chaja-f6b77c.netlify.app/500"> 500 (/500)</a></li>
+
+        For localhost: 
+        <li> <a href="http://localhost:3000/login"> Login (/login)</a></li>
+        <li> <a href="http://localhost:3000/registration"> Registration (/registration) </a></li>
+        <li> <a href="http://localhost:3000/chats"> Chats (/chats) </a></li>
+        <li> <a href="http://localhost:3000/profile"> Profile (/profile) </a></li>
+        <li> <a href="http://localhost:3000/settings"> Settings (/settings) </a></li>
+        <li> <a href="http://localhost:3000/passwordchange"> Change Password (/passwordchange) </a></li>
+        <li> <a href="http://localhost:3000/404"> 404 (/404)</a></li>
+        <li> <a href="http://localhost:3000/500"> 500 (/500)</a></li>
     </ul>
 `
